ce93b499e84db399749e9a1174ce6f9f5bc1cd9a
mucyoelise <mucyo.bryant@gmail.com>
2025-01-09 16:48:22 +0200

    Improving-functionality

--- mainProj.py
+++ mainProj.py
@@ -7,8 +7,12 @@ print(f"\n{pyfiglet.Figlet(font='ansi_regular').renderText('ZODIAC APP')}", end=
 print("~~ Do you know your birthdate? I can tell you which zodiac sign you fall under!😊 ~~\n")
 
 if input("Wanna provide your birth date? (yes/no): ").lower() in ['yes','y']:
-    
-    user = ZodiacSign(input('Enter your Date of Birth in ISO_FORMAT (YEAR-MONTH-DAY): '))
+    while True:
+        try:
+            user = ZodiacSign(input('Enter your Date of Birth in ISO_FORMAT (YEAR-MONTH-DAY): '))
+            break
+        except Exception:
+            continue
     print(user.getZodSign())
 
 else: print('Thank you!')
